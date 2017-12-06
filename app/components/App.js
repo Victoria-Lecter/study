1d110ba29a59d9703d1bb341ebcd84f5d571ad44
Simon <pechunka@11@gmail.com>
2017-12-06 23:01:14 +0100

    Created Navbar and Footer with all actions and stores. No yet configurated Socket.io

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Navbar from './Navbar';
+import Footer from './Footer';
+
 export default class App extends React.Component{
 	render() {
 		return(
 			<div>
+				<Navbar history={this.props.history} />
 				{this.props.children}
+				<Footer />
 			</div>
 		)
 	}
