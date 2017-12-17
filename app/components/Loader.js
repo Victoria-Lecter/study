3b697325293972ec6e2c21ead6433eb398978360
Daniel Khoroshko <rasdaniil@gmail.com>
2017-12-18 00:48:07 +0200

    + nodemon as devDep

---
+++ app/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Loader = () => (
+  <div style={style}>Loading!</div>
+);
+
+const style = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+};
+
